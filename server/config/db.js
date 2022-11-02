c0f87dad6858765c2b60764cc04b2b6a2a336eb6
harijetti <jettiharikrishna@gmail.com>
2022-11-02 10:32:46 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+// const username = "artrymus";
+// const password = "#hakr@RE0508";
+// const cluster = "Cluster0"
+// const dbname = "web";
+
+
+
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://127.0.0.1/books229"
+  "URI": "mongodb+srv://artrymus:w1hPySDoR5eYdEQr@cluster0.v7jug7m.mongodb.net/web?retryWrites=true&w=majority"
+};
